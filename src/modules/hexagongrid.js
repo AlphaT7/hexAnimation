3c8c8a244c032931b89241d9f0d88b54737886d4
AlphaT7 <7742643+AlphaT7@users.noreply.github.com>
2024-10-22 21:26:16 -0400

    fixed the opacity issue;

--- src/modules/hexagongrid.js
+++ src/modules/hexagongrid.js
@@ -6,53 +6,60 @@ export default class HexagonGrid {
     this.centerX = canvas.centerX;
     this.centerY = canvas.centerY;
     this.hexagonExpand = true;
-    this.hexagonExpandRate = 2;
+    this.hexagonExpandRate = 2.5;
     this.hexagonMaximumRadius = canvas.hexRadius;
+    // this.opacityStep must be relative to its negative start value
+    // and it must also increase at a rate relative to the number of frames
+    // it takes for the expanding hexagons to reach this.hexagonMaximumRadius;
+    this.opacityStep =
+      1.5 / (this.hexagonMaximumRadius / this.hexagonExpandRate);
     this.spaceBetweenHexagons = 7;
     this.hexagonFocus = 0;
+    this.updateHexagon = false;
 
     this.hexagonProperties = [
+      // starting with a negative opacity value gives the hexagons a "flickering" appearance when it becomes visible;
       {
         centerX: this.calculateX(240),
         centerY: this.calculateY(240),
         radius: 0,
-        opacity: 0,
+        opacity: -0.5,
       },
       {
         centerX: this.calculateX(300),
         centerY: this.calculateY(300),
         radius: 0,
-        opacity: 0,
+        opacity: -0.5,
       },
       {
         centerX: this.calculateX(0),
         centerY: this.calculateY(0),
         radius: 0,
-        opacity: 0,
+        opacity: -0.5,
       },
       {
         centerX: this.calculateX(60),
         centerY: this.calculateY(60),
         radius: 0,
-        opacity: 0,
+        opacity: -0.5,
       },
       {
         centerX: this.calculateX(120),
         centerY: this.calculateY(120),
         radius: 0,
-        opacity: 0,
+        opacity: -0.5,
       },
       {
         centerX: this.calculateX(180),
         centerY: this.calculateY(180),
         radius: 0,
-        opacity: 0,
+        opacity: -0.5,
       },
       {
         centerX: this.centerX,
         centerY: this.centerY,
         radius: 0,
-        opacity: 0,
+        opacity: -0.5,
       },
     ];
 
@@ -83,6 +90,26 @@ export default class HexagonGrid {
 
   update() {
     this.hexagonArray = [];
+
+    // set a brief delay between changing the "this.hexagonExpand" integer polarity
+    this.updateHexagon = false;
+    if (this.hexagonProperties[0].radius <= 0) {
+      setTimeout(() => {
+        this.hexagonExpand = true;
+        this.updateHexagon = true;
+      }, 500);
+    } else {
+      this.updateHexagon = true;
+    }
+    if (this.hexagonProperties[6].radius >= this.hexagonMaximumRadius) {
+      setTimeout(() => {
+        this.hexagonExpand = false;
+        this.updateHexagon = true;
+      }, 500);
+    } else this.updateHexagon = true;
+    if (!this.updateHexagon) return;
+
+    // the update functionality;
     this.hexagonProperties.forEach((hexagon, i) => {
       if (this.hexagonExpand) {
         // calculate the individual hexagon radius while expanding;
@@ -102,17 +129,11 @@ export default class HexagonGrid {
 
         // calculate the individual hexagon opacity while expanding;
         if (hexagon.opacity < 1 && this.hexagonFocus == i) {
-          hexagon.opacity += 0.05;
+          hexagon.opacity += this.opacityStep;
         }
-
-        // set the hexagonExpand variable;
-        this.hexagonExpand =
-          this.hexagonProperties[6].radius >= this.hexagonMaximumRadius
-            ? false
-            : true;
       } else {
         // calculate the individual hexagon radius while contracting;
-        if (hexagon.radius >= 0 && this.hexagonFocus == i)
+        if (hexagon.radius > 0 && this.hexagonFocus == i)
           hexagon.radius -= this.hexagonExpandRate;
 
         // calculate which hexagon is being rendered;
@@ -120,13 +141,9 @@ export default class HexagonGrid {
           this.hexagonFocus--;
 
         // calculate the individual hexagon opacity while expanding;
-        if (hexagon.opacity >= 1 && this.hexagonFocus == i) {
-          hexagon.opacity -= 0.05;
+        if (hexagon.opacity > -0.2 && this.hexagonFocus == i) {
+          hexagon.opacity -= this.opacityStep;
         }
-
-        // set the hexagonExpand variable;
-        this.hexagonExpand =
-          this.hexagonProperties[0].radius <= 0 ? true : false;
       }
 
       let centerX = hexagon.centerX;
